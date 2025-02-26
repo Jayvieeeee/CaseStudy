ae537a09cc2ed6d0331d9081d485def02f3a6a63
Jayvieeeee <jayvieletada@gmail.com>
2025-02-26 20:45:17 +0800

    Delete CustomAuth directory

--- CustomAuth/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomAuth.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
